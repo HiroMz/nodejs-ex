1d232e97994c5790d019f2df4bdda1430c1c3e50
HiroMz <hirotaka.uiji@mizuho-ir.co.jp>
2019-07-24 13:49:59 +0900

    nodejsのテスト

--- tests/app_test.js
+++ tests/app_test.js
@@ -29,3 +29,15 @@ describe('Basic routes tests', function() {
 
     })
 })
+
+//地図を作成
+var myap = L.map('map');
+
+//タイルレイヤーを作成し、地図にセットする
+L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">国土地理院</a>',
+}).addTo(mymap);
+
+//地図の中心座標とズームレベルを設定する
+mmap.setView([37.508106, 139.930239], 13);
